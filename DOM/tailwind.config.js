9f0e94113c2dfedb15533cf2baf8be1e9ec06144
AkibShahrear <akibsedu@gmail.com>
2022-12-02 12:32:54 +0600

    update

--- DOM/tailwind.config.js
+++ DOM/tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "./login.html",
     "./signup.html",
     "./create-post.html",
+    "./Recipes.html",
+    "./About.html",
+    "./Tags.html",
+    "./beef-page.html",
+    "./Breakfast-page.html",
+    "./Carrots.html",
+    "./Food.html",
   ],
   theme: {
     extend: {
@@ -14,6 +21,9 @@ module.exports = {
           100: "#E2E2D5",
           200: "#888883",
         },
+      },
+      backgroundImage: {
+        main: "url('./img/main.jpg')",
       },
     },
   },
